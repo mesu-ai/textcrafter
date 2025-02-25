1f296ddc881c9586ff6b6ce6a81dd13f580bc2cb
mesu-ai <mesu.s.ss@gmail.com>
2025-02-25 10:23:50 +0600

    Autometed Server Image Upload

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -12,51 +12,22 @@ function App() {
 
   const handleImagaUpload = async (file: File): Promise<string> => {
     const formData = new FormData();
-    formData.append("File", file);
-
-
-    // const reader= new FileReader();
-    // reader.readAsDataURL(file);
-
-    // const base64 = await new Promise<string>((resolve, reject)=>{
-    //   reader.onload = () => resolve(reader.result as string);
-    //   reader.onerror = reject;
-    // })
-
-    formData.append("MediaStorageInfo.Name", file.name);
-    formData.append("MediaStorageInfo.ValidExt", ".jpg,.jpeg,.png");
-    formData.append("MediaStorageInfo.UseFor", "category");
-
-
+    formData.append("image", file);
     console.log({ formData, file });
 
-        // const MediaStorageInfo = {
-        //   Id: 0,
-        //   File: base64,
-        //   Name: file.name,
-        //   ValidExtensions: ".jpg,.jpeg,.png",
-        //   UseFor:"category",
-        // };
-
-    const response = await fetch(
-      "https://prod.saraemart.com/api/Media/CreateOrUpdateFile",
-      {
-        method: "POST",
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI4MSIsImVtYWlsIjoiMCIsInVuaXF1ZV9uYW1lIjoiU2FSYSBBZG1pbiIsImNlcnRzZXJpYWxudW1iZXIiOiI4MSIsInJvbGUiOiIxIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbW9iaWxlcGhvbmUiOiIwIiwibmJmIjoxNzQwMzc2ODI0LCJleHAiOjE3NDI5Njg4MjQsImlhdCI6MTc0MDM3NjgyNH0.7IGiPjZqGlbpsFykcz1YmizlF327yQtRh63rM26FeE2erzoOlbYZRbGDsrg72MgwwEdc4egH6CcVDX8nkXyAyw",
-        },
-        body: formData,
-      }
-    );
-
+    const response = await fetch("/api/upload-image", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer token",
+      },
+      body: formData,
+    });
     const data = await response.json();
-    console.log(data);
-
-    console.log(`https://prod.saraemart.com${data.optional}`)
-  
-    return `https://prod.saraemart.com${data.optional}`;
+    console.log({ data });
+    return data.imageUrl;
   };
+
+
 
   const handleImageDelete = async (imgSrc: string) => {
     console.log({ imgSrc });
@@ -68,7 +39,6 @@ function App() {
     console.log({ data });
   };
 
-  // fetch(`${apiEndpoint?.deleteImage}?src=${encodeURIComponent(imgSrc)}`, { method: 'DELETE' });
 
   return (
     <div>
